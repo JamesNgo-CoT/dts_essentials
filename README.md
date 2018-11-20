543eca5832dfbbbd80b64218f2ce4585f55d4875
James Ngo <James.Ngo@toronto.ca>
2018-11-20 18:21:17 -0500

    Scaffold Cot App

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# dts_essentials
+dts_essentials
+===========
+Describe your app.
+
+For embedded apps:
+------------------
+This app can be embedded on COT Wordpress pages with:
+
+`[cot_app app="dts_essentials"][/cot_app]`
+
+Here is the application.json definition
+
+`{"id": "dts_essentials", "title": "My App Name", "description": "Describe your app."}`
---
+++ src/scripts/main.js
@@ -0,0 +1,21 @@
+// The main javascript file for dts_essentials.
+// IMPORTANT:
+// Any resources from this project should be referenced using SRC_PATH preprocessor var
+// Ex: let myImage = '/*@echo SRC_PATH*//img/sample.jpg';
+
+$(function () {
+  if (window['cot_app']) { //the code in this 'if' block should be deleted for embedded apps
+    const app = new cot_app("dts_essentials",{
+      hasContentTop: false,
+      hasContentBottom: false,
+      hasContentRight: false,
+      hasContentLeft: false,
+      searchcontext: 'INTRA'
+    });
+
+    app.setBreadcrumb([
+      {"name": "dts_essentials", "link": "#"}
+    ]).render();
+  }
+  let container = $('#dts_essentials_container');
+});
